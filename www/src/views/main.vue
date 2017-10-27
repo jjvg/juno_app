378517b1c4b4e0ddde9fdcebfea06fe0228a5159
jjvg <jjvg_12@hotmail.com>
2017-10-27 19:39:33 -0400

    listo

---
+++ www/src/views/main.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="app">
+	<div class="container">
+		<div class="row">
+			<div class="col s12 m6">
+					<p>Bienvenido a Red Social Juno</p>
+			
+			<router-link to="/prueba"><button class="waves-effect waves-light btn">Registrarse</button></router-link>
+					</div>
+			</div>
+
+			</div>
+	
+  	<div id="deviceready" class="blink">
+  		<p class="event listening"></p>
+  		<p class="event received"></p>
+  	</div>	
+</div>
+		
+   
+</template>
+<script>
+export default {
+  name: 'main'
+}
+</script>
